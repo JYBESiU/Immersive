da8659738c394544829c1ed36047bf35d35934f3
tommyjr1 <tommyjr1@naver.com>
2021-07-10 23:07:18 +0900

    GameActivity/기본 프론트엔드

--- app/src/main/java/com/example/thekaist/GameActivity.java
+++ app/src/main/java/com/example/thekaist/GameActivity.java
@@ -2,13 +2,21 @@ package com.example.thekaist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class GameActivity extends AppCompatActivity {
     String ask;
     String accept;
+
+    private TextView target, ply1, ply1scr, ply2, ply2scr;
+    private RecyclerView recyclerView;
+    //어댑터 필요
+    private ImageButton buzzer, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +27,42 @@ public class GameActivity extends AppCompatActivity {
         ask = intent.getExtras().getString("ask");
         accept = intent.getExtras().getString("accept");
 
-        TextView text_ask = findViewById(R.id.text_ask);
-        TextView text_accept = findViewById(R.id.text_accept);
+        target = findViewById(R.id.target);
+        ply1 = findViewById(R.id.ply1);
+        ply1scr = findViewById(R.id.ply1score);
+        ply2 = findViewById(R.id.ply2);
+        ply2scr = findViewById(R.id.ply2score);
 
-        text_ask.setText(ask);
-        text_accept.setText(accept);
+        recyclerView = findViewById(R.id.show_cards);
+        buzzer = findViewById(R.id.buzzer);
+        pass = findViewById(R.id.pass);
+
+        ply1.setText(ask);
+        ply2.setText(accept);
+
+        buzzer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak();
+            }
+        });
+
+        pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sayPass();
+            }
+        });
+
+
+
+
+
+    }
+
+    private void sayPass() {
+    }
+
+    private void speak() {
     }
 }
